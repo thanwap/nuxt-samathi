bfede3f0a7909b29aeb9b8b4ec29c420101bdda6
thanwap <dhunwa_p@hotmail.com>
2020-12-09 17:10:50 +0700

    initial

---
+++ src/layouts/default.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-app-bar fixed app>
+      <template>
+        <v-tabs>
+          <v-tab v-for="(item, i) in items" :key="i" :to="item.to" router>{{
+            item.title
+          }}</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          title: 'ป้ายชื่ออาจารย์',
+          to: '/',
+        },
+        {
+          title: 'อาจารย์',
+          to: '/teacher',
+        },
+        {
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
